bbc159a6a4ffebecaa2000e5615ab907c1481010
MohomedFazil <mfazilhack@gmail.com>
2025-06-25 22:14:53 +0530

    commit

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="text-center mt-32">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="text-lg mt-4">Sorry, the page you're looking for doesn't exist.</p>
+    </div>
+  );
+}
